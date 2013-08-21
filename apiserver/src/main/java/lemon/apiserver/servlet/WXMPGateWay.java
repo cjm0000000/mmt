a3a93ab3d653cdc637be3ff7161c53937ac7e1b7
Lemon <15168235743@126.com>
2013-08-21 14:43:23 +0800

    add gateway

---
+++ apiserver/src/main/java/lemon/apiserver/servlet/WXMPGateWay.java
@@ -0,0 +1,33 @@
+package lemon.apiserver.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The gateway of weixin
+ * @author lemzhang
+ *
+ */
+public class WXMPGateWay extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.print("hello.");
+	}
+
+
+}
---
+++ apiserver/src/test/java/lemon/apiserver/Test.java
@@ -0,0 +1,18 @@
+package lemon.apiserver;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * 
+ * @author lemon
+ *
+ */
+@RunWith(JUnit4.class)
+public class Test {
+
+	@org.junit.Test
+	public void run1(){
+		
+	}
+}
