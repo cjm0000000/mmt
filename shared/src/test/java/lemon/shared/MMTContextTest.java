45f1c7aa0b0286bfc1816d6961d20c908949f4d8
cjm0000000 <green120@126.com>
2013-09-03 22:30:29 +0800

    nothing.

--- shared/src/test/java/lemon/shared/MMTContextTest.java
+++ shared/src/test/java/lemon/shared/MMTContextTest.java
@@ -11,18 +11,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @RunWith(JUnit4.class)
 public class MMTContextTest {
-	private MMTContext content;
 	@Before
 	public void init() {
 		String[] resource = { "classpath:spring-db.xml",
 				"classpath:spring-dao.xml", "classpath:spring-service.xml" };
 		ApplicationContext acx = new ClassPathXmlApplicationContext(resource);
-		content = acx.getBean(MMTContext.class);
-		assertEquals(acx, content.getApplicationContext());
+		assertEquals(acx, MMTContext.getApplicationContext());
 	}
 	@Test
 	public void testMMT(){
-		ApplicationContext acx = content.getApplicationContext();
+		ApplicationContext acx = MMTContext.getApplicationContext();
 		assertNotNull(acx.getBean(MMTContext.class));
 	}
 }
