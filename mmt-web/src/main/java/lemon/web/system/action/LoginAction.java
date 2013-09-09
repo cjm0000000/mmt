d52ff1c63340b9726a9577f96e9e96de05bef3cc
lemon <green120@126.com>
2013-09-09 17:34:45 +0800

    add spring MVC.

---
+++ mmt-web/src/main/java/lemon/web/system/action/LoginAction.java
@@ -0,0 +1,32 @@
+package lemon.web.system.action;
+
+import lemon.web.system.bean.User;
+import lemon.web.system.mapper.UserMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * For login business
+ * @author lemon
+ * @version 1.0
+ *
+ */
+@Controller
+public class LoginAction {
+	private static final String SUCCESS = "success";
+	@Autowired
+	private UserMapper userMapper;
+	
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public String login(@ModelAttribute("user")User user){
+		User u = userMapper.checkLogin(user.getUser_name(), user.getPassword());
+		if(u != null){
+			//TODO set session or generate token
+		}
+		return SUCCESS;
+	}
+}
