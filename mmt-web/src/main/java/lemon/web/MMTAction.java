14e1eb67628f136d2be9faaf03aac5f9e44b6b57
lemon <green120@126.com>
2013-09-12 01:02:00 +0800

    add login logic.

---
+++ mmt-web/src/main/java/lemon/web/MMTAction.java
@@ -0,0 +1,5 @@
+package lemon.web;
+
+public class MMTAction {
+	//FIXME 封装Action：分页，错误信息，提示信息，跳转路径等
+}
--- mmt-web/src/main/java/lemon/web/system/action/LoginAction.java
+++ mmt-web/src/main/java/lemon/web/system/action/LoginAction.java
@@ -2,7 +2,11 @@ package lemon.web.system.action;
 
 import javax.servlet.http.HttpSession;
 
+import lemon.shared.util.SecureUtil;
+import lemon.web.log.bean.LoginLog;
 import lemon.web.system.bean.User;
+import lemon.web.system.bean.UserConfig;
+import lemon.web.system.mapper.UserConfigMapper;
 import lemon.web.system.mapper.UserMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +24,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginAction {
 	private static final String HOME_PAGE = "redirect:/";
+	private static final String ENCRYPY_KEY = "EncryptKey";
 	@Autowired
 	private UserMapper userMapper;
-	
+	@Autowired
+	private UserConfigMapper userConfigMapper;
+	//TODO 添加
 	
 	/**
 	 * verify user login
@@ -33,7 +40,23 @@ public class LoginAction {
 	 */
 	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public ModelAndView login(String user_name,String password,HttpSession session){
-		User user = userMapper.checkLogin(user_name,password);
+		int user_id = userMapper.getUserIdByName(user_name);
+		if(user_id <= 0){
+			//用户不存在
+		}
+		//获取encryptKey
+		UserConfig encryptKeyItem =  userConfigMapper.getItem(user_id,ENCRYPY_KEY);
+		if(null == encryptKeyItem){
+			//TODO handle error
+		}
+		//验证用户名和密码
+		User user = userMapper.checkLogin(user_name,SecureUtil.aesEncrypt(password, encryptKeyItem.getValue()));
+		//保存日志
+		LoginLog log = new LoginLog();
+		log.setLoginip("");
+		log.setUser_name(user_name);
+		log.setUser_id(user_id);
+		//TODO 设置LOG
 		if(user != null){
 			session.setAttribute("user", user);
 		}
