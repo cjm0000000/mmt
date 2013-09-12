2e524bfd30c1ac5c67c54da8d47f9d4111d68057
lemon <green120@126.com>
2013-09-12 14:15:23 +0800

    增加Session验证功能。

--- mmt-web/src/main/java/lemon/web/base/MMTHandlerInterceptor.java
+++ mmt-web/src/main/java/lemon/web/base/MMTHandlerInterceptor.java
@@ -3,6 +3,10 @@ package lemon.web.base;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lemon.web.HomeAction;
+import lemon.web.system.action.LoginAction;
+import lemon.web.system.bean.User;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -24,9 +28,17 @@ public class MMTHandlerInterceptor implements HandlerInterceptor {
 	 */
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		System.out.println("Before execute");
-		logger.debug("Before execute: " + handler);
-		return false;
+		//对于主页和登录页面不做处理
+		if(handler instanceof LoginAction || handler instanceof HomeAction)
+			return true;
+		User user = (User) request.getSession().getAttribute("user");
+		//跳到登录页面
+		if(null == user){
+			logger.debug("用户SESSION失效");
+			response.sendRedirect("/index");
+			return false;
+		}
+		return true;
 	}
 
 	@Override
