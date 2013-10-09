abe92cd0d2f6aa05e2ef5f03ed84b48952f71ea3
lemon <green120@126.com>
2013-10-10 00:15:18 +0800

    增加微信接口表单页

--- mmt-web/src/main/webapp/js/manage/customer/information.js
+++ mmt-web/src/main/webapp/js/manage/customer/information.js
@@ -1,30 +1,19 @@
 $(function() {
-	$modal = $('#cust-ajax-modal');
-	
-	//监听加载添加页面事件
-	$('#cust-add').on('click', function(){
-		loadPage(url_showPage, {cust_id:0}, $modal);
+	//监听接口开关
+	$('#interface-switch').on('switch-change', function (e, data) {
+	    if(data.value){
+	    	$("#form-main").css('display','inline');
+	    }else{
+	    	$("#form-main").css('display','none');
+	    }
 	});
 	
-	//监听加载编辑页面事件
-	$('#cust-update').on('click', function(){
-		var cust_id = getSelectedValues('cust_id');
-		loadPage(url_showPage, {cust_id:cust_id}, $modal);
-	});
-	
-	//监听保存数据事件
-	$modal.on('click', '.save', function(){
-		var params_save = {
-			cust_id		: $('#cust_id').val(),
-			cust_name	: $('#cust_name').val(),
-			memo		: $('#memo').val()
-	 	};
-		mmtPost(url_save, params_save, $modal.find('.modal-body'), $modal.find('.modal-pre-body'));
-	});
-	
-	//监听删除事件
-	$('#cust-delete').on('click', function(){
-		var cust_id = getSelectedValues("cust_id");
-		mmtPost(url_delete, {cust_id:cust_id}, $('body'), $('.panel-heading'));
+	//监听服务号开关
+	$('#service-no').on('switch-change', function (e, data) {
+	    if(data.value){
+	    	$("#service-require").css('display','inline');
+	    }else{
+	    	$("#service-require").css('display','none');
+	    }
 	});
 });
