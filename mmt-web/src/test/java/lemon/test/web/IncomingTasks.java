cd74431014db0e9af50f100e73f11c12dffb2593
lemon <green120@126.com>
2013-11-20 01:13:20 +0800

    更新Task

--- mmt-web/src/test/java/lemon/test/web/IncomingTasks.java
+++ mmt-web/src/test/java/lemon/test/web/IncomingTasks.java
@@ -10,8 +10,5 @@ public abstract class IncomingTasks {
 	
 	//TODO [2013-11-19] 开启Spring事物
 	//TODO [2013-11-19] Spring Security 重新登录后，缓存的targetURL有问题
-	//TODO [2013-11-19] 翻页代码增加查询过滤参数
 	//TODO [2013-11-19] BS3 悬浮消息框（包括错误消息和提示消息）
-	//TODO [2013-11-19] list方法的redirect存在问题。
-	//TODO [2013-11-19] 删除最后一页全部记录，需要路由到前一页。
 }
