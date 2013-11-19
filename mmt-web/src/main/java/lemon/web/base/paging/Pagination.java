d69e7f0469c3bbf7f0bf46d8d6174f99dd366a91
lemon <green120@126.com>
2013-11-19 23:39:47 +0800

    分页增加查询条件；

--- mmt-web/src/main/java/lemon/web/base/paging/Pagination.java
+++ mmt-web/src/main/java/lemon/web/base/paging/Pagination.java
@@ -7,30 +7,60 @@ package lemon.web.base.paging;
  *
  */
 public class Pagination {
+	/** 当前页 */
 	private int currentPage;
-	
+	/** 每页记录数 */
 	private int pageSize;
-	
+	/** 总记录数 */
 	private int resCount;
+	/** 分页查询条件 */
+	private String filters;
 	
 	public Pagination(int currentPage, int pageSize, int resCount){
 		this.currentPage = currentPage;
 		this.pageSize = pageSize;
 		this.resCount = resCount;
 	}
+	
+	public Pagination(int currentPage, int pageSize, int resCount, String filters){
+		this.currentPage = currentPage;
+		this.pageSize = pageSize;
+		this.resCount = resCount;
+		this.filters = filters;
+	}
 
+	/**
+	 * 获取当前页
+	 * @return
+	 */
 	public int getCurrentPage() {
 		return currentPage;
 	}
 
+	/**
+	 * 获取每页记录数
+	 * @return
+	 */
 	public int getPageSize() {
 		return pageSize;
 	}
 
+	/**
+	 * 获取记录数
+	 * @return
+	 */
 	public int getResCount() {
 		return resCount;
 	}
 	
+	/**
+	 * 获取分页查询条件
+	 * @return
+	 */
+	public String getFilters() {
+		return filters;
+	}
+
 	/**
 	 * 获取最后一页页码
 	 * @return
