743833e43e7cea8f48f29143f0130319359243b9
lemon <green120@126.com>
2013-11-19 09:37:26 +0800

    Add incoming tasks.

---
+++ mmt-web/src/test/java/lemon/test/web/IncomingTasks.java
@@ -0,0 +1,14 @@
+package lemon.test.web;
+
+/**
+ * 负责即将到来的任务
+ * @author lemon
+ * @version 1.1
+ *
+ */
+public abstract class IncomingTasks {
+	
+	//TODO [2013-11-19] 开启Spring事物
+	//TODO [2013-11-19] Spring Security 重新登录后，缓存的targetURL有问题
+	//TODO [2013-11-19] 翻页代码增加查询过滤参数
+}
