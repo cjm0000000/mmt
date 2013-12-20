7f3311a5882aa8716e2fad33aaf8c276841ef631
Lemon <green120@126.com>
2013-12-20 16:45:34 +0800

    Add JSON parser.

--- core/src/main/java/com/github/cjm0000000/mmt/core/parser/driver/SimpleJSONDriver.java
+++ core/src/main/java/com/github/cjm0000000/mmt/core/parser/driver/SimpleJSONDriver.java
@@ -1,5 +1,9 @@
 package com.github.cjm0000000.mmt.core.parser.driver;
 
+import org.apache.log4j.Logger;
+
+import com.alibaba.fastjson.JSON;
+
 /**
  * Simple JSON driver implement
  * @author lemon
@@ -7,5 +11,18 @@ package com.github.cjm0000000.mmt.core.parser.driver;
  *
  */
 public class SimpleJSONDriver {
-	//TODO implement JSON driver
+	private static final Logger logger = Logger.getLogger(SimpleJSONDriver.class);
+	
+	public String toJSON(Object obj){
+		if(logger.isDebugEnabled())
+			logger.debug("begin to generate JSON.");
+		return JSON.toJSONString(obj);
+	}
+	
+	public <T> Object fromJSON(String json, Class<T> type){
+		if(logger.isDebugEnabled())
+			logger.debug("begin to parse JSON.");
+		return JSON.parseObject(json, type);
+	}
+	
 }
