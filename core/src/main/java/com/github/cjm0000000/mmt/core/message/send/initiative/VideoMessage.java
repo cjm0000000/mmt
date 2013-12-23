b2a0d6abd2aa8fb2a4122ff001a4924b091cfdd2
Lemon <green120@126.com>
2013-12-23 15:48:15 +0800

    Add video message.

---
+++ core/src/main/java/com/github/cjm0000000/mmt/core/message/send/initiative/VideoMessage.java
@@ -0,0 +1,33 @@
+package com.github.cjm0000000.mmt.core.message.send.initiative;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.github.cjm0000000.mmt.core.message.MsgType;
+
+/**
+ * video message for initiative send
+ * @author lemon
+ * @version 2.0
+ *
+ */
+public class VideoMessage extends SimpleMessage {
+	@JSONField(name = MsgType.VIDEO)
+	private VideoNode video;
+	
+	public VideoMessage() {
+		super(MsgType.VIDEO);
+	}
+	
+	public VideoMessage(String media_id, String title, String description){
+		this();
+		this.video = new VideoNode(media_id, title, description);
+	}
+
+	public VideoNode getVideo() {
+		return video;
+	}
+
+	public void setVideo(VideoNode video) {
+		this.video = video;
+	}
+
+}
