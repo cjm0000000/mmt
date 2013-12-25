ffa5fd4152eddc4b6664a4eb07fd0360852e1ad9
Lemon <green120@126.com>
2013-12-25 15:14:16 +0800

    调整结构

--- mmt-shared/src/main/java/com/github/cjm0000000/mmt/shared/message/processor/AbstractPassiveEventProcessor.java
+++ mmt-shared/src/main/java/com/github/cjm0000000/mmt/shared/message/processor/AbstractPassiveEventProcessor.java
@@ -3,12 +3,16 @@ package com.github.cjm0000000.mmt.shared.message.processor;
 import org.apache.log4j.Logger;
 
 import com.github.cjm0000000.mmt.core.MmtException;
+import com.github.cjm0000000.mmt.core.config.MmtConfig;
 import com.github.cjm0000000.mmt.core.message.BaseMessage;
 import com.github.cjm0000000.mmt.core.message.event.EventType;
 import com.github.cjm0000000.mmt.core.message.event.KeyEvent;
 import com.github.cjm0000000.mmt.core.message.event.LocationEvent;
 import com.github.cjm0000000.mmt.core.message.event.ScanEvent;
 import com.github.cjm0000000.mmt.core.message.event.SimpleEvent;
+import com.github.cjm0000000.mmt.core.message.processor.PassiveMsgProcessor;
+import com.github.cjm0000000.mmt.core.message.recv.SimpleRecvMessage;
+import com.github.cjm0000000.mmt.core.service.MmtService;
 
 /**
  * passive event processor
@@ -16,38 +20,41 @@ import com.github.cjm0000000.mmt.core.message.event.SimpleEvent;
  * @version 2.0
  *
  */
-public abstract class AbstractPassiveEventProcessor extends AbstractPassiveMsgProcessor {
+abstract class AbstractPassiveEventProcessor implements PassiveMsgProcessor {
 	private static final Logger logger = Logger.getLogger(AbstractPassiveEventProcessor.class);
 	
 	/**
-	 * process event
-	 * @param event
+	 * get MMT configure
+	 * @param mmt_token
 	 * @return
 	 */
-	public final BaseMessage processEvent(SimpleEvent event){
+	protected abstract MmtConfig getConfig(String mmt_token);
+	
+	/**
+	 * process message
+	 * @param msg
+	 * @return
+	 */
+	protected abstract BaseMessage processMessage(SimpleRecvMessage msg);
+	
+	@Override
+	public final BaseMessage process(String mmt_token, MmtService recvMsg) {
+		MmtConfig cfg = getConfig(mmt_token);
+		if(cfg == null)
+			throw new MmtException("MMT config is null.");
 		if(logger.isDebugEnabled())
-			logger.debug("process event[eventType=" + event.getEventType() + "]");
-		EventType eventType = event.getEventType();
-		if(EventType.subscribe.equals(eventType)){//subscribe 
-			BaseMessage resMsg = doSubscribe(event);
-			if(event instanceof ScanEvent){
-				//参数过滤 qrscene_
-				ScanEvent scan = (ScanEvent) event;
-				scan.setEventKey(scan.getEventKey().replaceAll("qrscene_", ""));
-				return doScan(scan);
-			}
-			return resMsg;
-		}
-		if(EventType.unsubscribe.equals(eventType))
-			return doUnsubscribe(event);
-		if(EventType.scan.equals(eventType) && event instanceof ScanEvent)
-			return doScan((ScanEvent) event);
-		if(EventType.LOCATION.equals(eventType) && event instanceof LocationEvent)
-			return reportLocation((LocationEvent) event);
-		if(EventType.CLICK.equals(eventType) && event instanceof KeyEvent)
-			doClick((KeyEvent) event);
-		logger.error("No event type found!!!");
-		throw new MmtException("没有找到对应的事件。");
+			logger.debug("MMT configure is " + cfg);
+		
+		recvMsg.setCust_id(cfg.getCust_id());
+		recvMsg.setService_type(getServiceType());
+		
+		if(recvMsg instanceof SimpleRecvMessage) // process passive message
+			return processMessage((SimpleRecvMessage) recvMsg);
+		
+		if(recvMsg instanceof SimpleEvent) // process passive event
+			return processEvent((SimpleEvent) recvMsg);
+		
+		throw new MmtException("No such message instance.", new ClassCastException());
 	}
 	
 	/**
@@ -108,6 +115,37 @@ public abstract class AbstractPassiveEventProcessor extends AbstractPassiveMsgPr
 	protected BaseMessage reportLocation(LocationEvent event){
 		//TODO 完成地理位置上报功能
 		return null;
+	}
+	
+	/**
+	 * process event
+	 * @param event
+	 * @return
+	 */
+	private BaseMessage processEvent(SimpleEvent event){
+		if(logger.isDebugEnabled())
+			logger.debug("process event[eventType=" + event.getEventType() + "]");
+		EventType eventType = event.getEventType();
+		if(EventType.subscribe.equals(eventType)){//subscribe 
+			BaseMessage resMsg = doSubscribe(event);
+			if(event instanceof ScanEvent){
+				//参数过滤 qrscene_
+				ScanEvent scan = (ScanEvent) event;
+				scan.setEventKey(scan.getEventKey().replaceAll("qrscene_", ""));
+				return doScan(scan);
+			}
+			return resMsg;
+		}
+		if(EventType.unsubscribe.equals(eventType))
+			return doUnsubscribe(event);
+		if(EventType.scan.equals(eventType) && event instanceof ScanEvent)
+			return doScan((ScanEvent) event);
+		if(EventType.LOCATION.equals(eventType) && event instanceof LocationEvent)
+			return reportLocation((LocationEvent) event);
+		if(EventType.CLICK.equals(eventType) && event instanceof KeyEvent)
+			doClick((KeyEvent) event);
+		logger.error("No event type found!!!");
+		throw new MmtException("没有找到对应的事件。");
 	}
 	
 }
--- mmt-shared/src/main/java/com/github/cjm0000000/mmt/shared/message/processor/AbstractPassiveMsgProcessor.java
+++ mmt-shared/src/main/java/com/github/cjm0000000/mmt/shared/message/processor/AbstractPassiveMsgProcessor.java
@@ -3,10 +3,8 @@ package com.github.cjm0000000.mmt.shared.message.processor;
 import org.apache.log4j.Logger;
 
 import com.github.cjm0000000.mmt.core.MmtException;
-import com.github.cjm0000000.mmt.core.config.MmtConfig;
 import com.github.cjm0000000.mmt.core.message.BaseMessage;
 import com.github.cjm0000000.mmt.core.message.event.SimpleEvent;
-import com.github.cjm0000000.mmt.core.message.processor.PassiveMsgProcessor;
 import com.github.cjm0000000.mmt.core.message.recv.IVideo;
 import com.github.cjm0000000.mmt.core.message.recv.ImageMessage;
 import com.github.cjm0000000.mmt.core.message.recv.LinkMessage;
@@ -15,7 +13,6 @@ import com.github.cjm0000000.mmt.core.message.recv.SimpleRecvMessage;
 import com.github.cjm0000000.mmt.core.message.recv.TextMessage;
 import com.github.cjm0000000.mmt.core.message.recv.weixin.VoiceMessage;
 import com.github.cjm0000000.mmt.core.message.recv.yixin.AudioMessage;
-import com.github.cjm0000000.mmt.core.service.MmtService;
 
 /**
  * passive message processor
@@ -23,15 +20,8 @@ import com.github.cjm0000000.mmt.core.service.MmtService;
  * @version 2.0
  *
  */
-public abstract class AbstractPassiveMsgProcessor implements PassiveMsgProcessor {
+public abstract class AbstractPassiveMsgProcessor extends AbstractPassiveEventProcessor {
 	private static final Logger logger = Logger.getLogger(AbstractPassiveMsgProcessor.class);
-	
-	/**
-	 * get MMT configure
-	 * @param mmt_token
-	 * @return
-	 */
-	protected abstract MmtConfig getConfig(String mmt_token);
 	
 	/**
 	 * process event
@@ -39,26 +29,6 @@ public abstract class AbstractPassiveMsgProcessor implements PassiveMsgProcessor
 	 * @return
 	 */
 	protected abstract BaseMessage processEvent(SimpleEvent event);
-	
-	@Override
-	public final BaseMessage process(String mmt_token, MmtService recvMsg) {
-		MmtConfig cfg = getConfig(mmt_token);
-		if(cfg == null)
-			throw new MmtException("MMT config is null.");
-		if(logger.isDebugEnabled())
-			logger.debug("MMT configure is " + cfg);
-		
-		recvMsg.setCust_id(cfg.getCust_id());
-		recvMsg.setService_type(getServiceType());
-		
-		if(recvMsg instanceof SimpleRecvMessage) // process passive message
-			return processMessage((SimpleRecvMessage) recvMsg);
-		
-		if(recvMsg instanceof SimpleEvent) // process passive event
-			return processEvent((SimpleEvent) recvMsg);
-		
-		throw new MmtException("No such message instance.", new ClassCastException());
-	}
 	
 	/**
 	 * 处理语音消息
@@ -114,7 +84,7 @@ public abstract class AbstractPassiveMsgProcessor implements PassiveMsgProcessor
 	 * @param msg
 	 * @return
 	 */
-	private final BaseMessage processMessage(SimpleRecvMessage msg){
+	public final BaseMessage processMessage(SimpleRecvMessage msg){
 		if(logger.isDebugEnabled())
 			logger.debug("process message[msgType=" + msg.getMsgType() + "]");
 		if(msg instanceof TextMessage)
